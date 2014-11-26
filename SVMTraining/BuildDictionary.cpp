24b4d1299c489c7864b4f10369be1a619979ddf4
kt-9 <nkanapin@gmail.com>
2014-11-26 18:52:40 +0600

    Trained classifiers for 5 classes. Located in folder classifiers/

--- SVMTraining/BuildDictionary.cpp
+++ SVMTraining/BuildDictionary.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 #include <cstring>
 #include <string>
+#include <fstream>
+#include <set>
+#include <map>
 
 
 #include <opencv2/opencv.hpp>
@@ -12,134 +15,233 @@
 
 using namespace cv;
 
-const int NUMBER_OF_CLASSES = 4;
 const int DICTIONARY_SIZE = 200;
 
-char file_name[81] = "images/list.txt";
+std::vector<std::string> classes_names;
 
-void train_images() {
-    //Step 2 - Obtain the BoF descriptor for given image/video frame. 
+std::map<std::string, Mat> classes_training_data;
 
-    //prepare BOW descriptor extractor from the dictionary    
-    Mat dictionary; 
-    FileStorage fs("dictionary.yml", FileStorage::READ);
-    fs["vocabulary"] >> dictionary;
-    fs.release();    
+
+
+void trainImages(string listFileName, string vocabularyFileName = "vocabulary.yml") {
     
-
-
-    //create a nearest neighbor matcher
-    Ptr<DescriptorMatcher> matcher(new FlannBasedMatcher);
-    //create Sift feature point extracter
-    Ptr<FeatureDetector> detector(new SiftFeatureDetector());
-    //create Sift descriptor extractor
-    Ptr<DescriptorExtractor> extractor(new SiftDescriptorExtractor);    
-    //create BoF (or BoW) descriptor extractor
-    BOWImgDescriptorExtractor bowDE(extractor,matcher);
-    //Set the dictionary with the vocabulary we created in the first step
-    bowDE.setVocabulary(dictionary);
-
-    /*
-    for (int i=0;i<classes_names.size();i++) {
-       string class_ = classes_names[i];
-       cout << " training class: " << class_ << ".." << endl;
-             
-       Mat samples(0,response_cols,response_type);
-       Mat labels(0,1,CV_32FC1);
-             
-       //copy class samples and label
-       cout << "adding " << classes_training_data[class_].rows << " positive" << endl;
-       samples.push_back(classes_training_data[class_]);
-       Mat class_label = Mat::ones(classes_training_data[class_].rows, 1, CV_32FC1);
-       labels.push_back(class_label);
-             
-       //copy rest samples and label
-       for (map<string,Mat>::iterator it1 = classes_training_data.begin(); it1 != classes_training_data.end(); ++it1) {
-          string not_class_ = (*it1).first;
-          if(not_class_.compare(class_)==0) continue; //skip class itself
-          samples.push_back(classes_training_data[not_class_]);
-          class_label = Mat::zeros(classes_training_data[not_class_].rows, 1, CV_32FC1);
-          labels.push_back(class_label);
-       }
-        
-       cout << "Train.." << endl;
-       Mat samples_32f; samples.convertTo(samples_32f, CV_32F);
-       if(samples.rows == 0) continue; //phantom class?!
-       CvSVM classifier;
-       classifier.train(samples_32f,labels);
-     
-       //do something with the classifier, like saving it to file
-    }
-
-    */
-
-}
-
-
-int main() {
-
-    
-    Mat input;    
-
-    
-    //To store the keypoints that will be extracted by SIFT
-    vector<KeyPoint> keypoints;
-    //To store the SIFT descriptor of current image
-    Mat descriptor;
-    //To store all the descriptors that are extracted from all the images.
-    Mat featuresUnclustered;
-    //The SIFT feature extractor and descriptor
-    SIFT sift;    
-
-    // Open file with names
-    freopen(file_name, "r", stdin);
-
-    char* image_name = new char[81];
-    char* dir_name = new char[21];
-
-    for (int i = 0; i < NUMBER_OF_CLASSES ; i ++) 
-    {        
-        scanf("%s", dir_name);
-        int len = strlen(dir_name);
-        dir_name[len - 1] = 0;
-        printf("Processing %s\n", dir_name);
-        for (int j = 1 ; j <= 10 ; j++) 
-        {
-            scanf("%s", file_name);
-            sprintf(image_name, "images/%s/%s", dir_name, file_name);
-
-            puts(image_name);
-            
-            input = imread(image_name, CV_LOAD_IMAGE_GRAYSCALE); //Load as grayscale                
-            //detect feature points
-            sift.detect(input, keypoints);
-
-            printf("Found %d keypoints\n", keypoints.size());
-            //compute the descriptors for each keypoint
-            sift.compute(input, keypoints, descriptor);        
-            //put the all feature descriptors in a single Mat object 
-            featuresUnclustered.push_back(descriptor);        
-        }
-        getchar();
-    }    
-
-    printf("In total %d rows\n", featuresUnclustered.rows);
-    
-
-
-    
-    //define Term Criteria
-    TermCriteria tc(CV_TERMCRIT_ITER, 100, 0.0001);
-    //retries number
-    int retries=1;
-    //necessary flags
-    int flags=KMEANS_RANDOM_CENTERS;
-    //Create the BoW (or BoF) trainer
-    BOWKMeansTrainer bowTrainer(DICTIONARY_SIZE,tc,retries,flags);
-    //cluster the feature vectors
-    Mat dictionary=bowTrainer.cluster(featuresUnclustered);    
-    //store the vocabulary
-    FileStorage fs("dictionary.yml", FileStorage::WRITE);
-    fs << "vocabulary" << dictionary;
+    Mat vocabulary;
+    FileStorage fs(vocabularyFileName.c_str(), FileStorage::READ);
+    fs["vocabulary"] >> vocabulary;
     fs.release();
+    
+    
+    SIFT detector;
+    Ptr<DescriptorExtractor> extractor = DescriptorExtractor::create("SIFT");
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
+
+    BOWImgDescriptorExtractor bowide(extractor, matcher);
+
+    bowide.setVocabulary(vocabulary);
+    
+    std::fstream in;
+    in.open(listFileName, std::fstream::in);
+    
+    std::string imageFileName;
+    Mat input;
+    std::vector<KeyPoint> keypoints;
+    Mat responseHistogram(1, bowide.descriptorSize(), CV_32FC1);
+    
+    while (in >> imageFileName)
+    {
+        int index = imageFileName.find('_');
+        
+        
+        std::string cur_class_name = imageFileName.substr(0, index);
+        
+        imageFileName = "images/" + imageFileName;
+        input = imread(imageFileName.c_str(), CV_LOAD_IMAGE_GRAYSCALE);
+        detector.detect(input, keypoints);
+        bowide.compute(input, keypoints, responseHistogram);
+        if (classes_training_data.count(cur_class_name) == 0) {
+            classes_names.push_back(cur_class_name);
+            //classes_training_data.create(0, responseHistogram.cols, responseHistogram.type());
+            
+        }
+        classes_training_data[cur_class_name].push_back(responseHistogram);
+        
+    }
+    
+    in.close();
+
+    for (int i = 0 ; i < classes_names.size() ; i++) {
+        string currentClass = classes_names[i];
+        std::cout << "Training class: " << currentClass << std::endl;
+        
+        
+        Mat samples(0, extractor->descriptorSize(), extractor->descriptorType());
+        Mat labels(0,1,CV_32FC1);
+        
+        std::cout << "Adding " << classes_training_data[currentClass].rows << " positive" << std::endl;
+        
+        samples.push_back(classes_training_data[currentClass]);
+        Mat class_label = Mat::ones(classes_training_data[currentClass].rows, 1, CV_32FC1);
+        labels.push_back(class_label);
+        
+        //copy rest samples and label
+        for (std::map<std::string,Mat>::iterator it1 = classes_training_data.begin();
+             it1 != classes_training_data.end(); ++it1)
+        {
+            std::string otherClass = (*it1).first;
+            if(otherClass.compare(currentClass)==0) continue; //skip class itself
+            
+            samples.push_back(classes_training_data[otherClass]);
+            class_label = Mat::zeros(classes_training_data[otherClass].rows, 1, CV_32FC1);
+            labels.push_back(class_label);
+        }
+        
+        std::cout << "Training.." << std::endl;
+        Mat samples_32f; samples.convertTo(samples_32f, CV_32F);
+        if(samples.rows == 0) continue; //phantom class?!
+        CvSVM classifier;
+        classifier.train(samples_32f, labels);
+        classifier.save(("classifiers/" + currentClass + ".xml").c_str());
+        
+    }
 }
+
+
+// TODO get list of files as a parameter
+void buildVocabulary(string listFileName) {
+    
+    std::cout << "Creating a vocabulary\n";
+    Mat input;
+    
+    
+    
+    vector<KeyPoint> keypoints;
+    
+    Mat descriptor;
+    
+    Mat featuresUnclustered;
+    
+    SIFT sift;
+    
+    
+    
+    std::fstream imageListFile;
+    imageListFile.open(listFileName, std::fstream::in);
+    
+    string imageFileName;
+    
+    
+    
+    while (imageListFile >> imageFileName)
+    {
+        int index = imageFileName.find('_');
+        
+        
+        std::string cur_class_name = imageFileName.substr(0, index);
+        
+        imageFileName = "images/" + imageFileName;
+        std::cout << "Processing " << imageFileName << std::endl;
+        
+        input = imread(imageFileName.c_str(), CV_LOAD_IMAGE_GRAYSCALE);
+        sift.detect(input, keypoints);
+        
+        std::cout << "Found " << keypoints.size() << " keypoints\n";
+        sift.compute(input, keypoints, descriptor);
+        
+        featuresUnclustered.push_back(descriptor);
+    }
+    
+    imageListFile.close();
+    
+    std::cout << "In total " << featuresUnclustered.rows << std::endl;
+    
+    
+    
+    
+    TermCriteria tc(CV_TERMCRIT_ITER, 100, 0.0001);
+    int retries = 1;
+    int flags = KMEANS_RANDOM_CENTERS;
+    BOWKMeansTrainer bowTrainer(DICTIONARY_SIZE, tc, retries, flags);
+    
+    Mat vocabulary = bowTrainer.cluster(featuresUnclustered);
+    
+    FileStorage fs("vocabulary.yml", FileStorage::WRITE);
+    fs << "vocabulary" << vocabulary;
+    fs.release();
+    
+    std::cout << "Saved a vocabulary to vocabulary.yml\n";
+}
+
+void testClassifiers() {
+    Mat vocabulary;
+    FileStorage fs("vocabulary.yml", FileStorage::READ);
+    fs["vocabulary"] >> vocabulary;
+    fs.release();
+    
+    
+    SIFT detector;
+    Ptr<DescriptorExtractor> extractor = DescriptorExtractor::create("SIFT");
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
+    
+    
+    BOWImgDescriptorExtractor bowide(extractor, matcher);
+    
+    bowide.setVocabulary(vocabulary);
+    
+    
+    const int classNumber = 5;
+    std::string class_names[classNumber] = {"congresshall", "shabyt", "vokzal", "hanshatyr", "triumf"};
+    CvSVM classifiers[classNumber];
+    for (int i = 0 ; i < classNumber ; i ++) {
+        
+        classifiers[i].load( ("classifiers/" + class_names[i] + ".xml").c_str() );
+        std::cout << "classifiers/" + class_names[i] + ".xml loaded." << std::endl;
+    }
+    string testImageName;
+    Mat input, response_hist, descriptor;
+    std::vector<KeyPoint> keypoints;
+    
+    while (std::cin >> testImageName) {
+        input = imread(testImageName.c_str(), CV_LOAD_IMAGE_GRAYSCALE);
+        
+        std::cout << "Loaded " + testImageName << input.rows << " x " << input.cols << std::endl;
+        
+        detector.detect(input, keypoints);
+        std::cout << "Found " << keypoints.size() << " keypoints\n";
+        //extractor->compute(input, keypoints, descriptor);
+        
+        bowide.compute(input, keypoints, response_hist);
+        std::cout << "Response hist " << response_hist.cols << std::endl;
+        
+        
+        float minf = FLT_MAX;
+        std::string bestMatch;
+        for (int i = 0 ; i < classNumber ; i ++) {
+            float res = classifiers[i].predict(response_hist,true);
+            if (res < minf) {
+                minf = res;
+                bestMatch = class_names[i];
+            }
+        }
+        std::cout <<testImageName << " is " << bestMatch << std::endl;
+    }
+}
+int main() {
+    
+    buildVocabulary("list2.txt");
+    trainImages("list2.txt");
+    testClassifiers();
+    
+    
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
