deded409768a9a2effcb2bda872e19e52db2adf7
kt-9 <nkanapin@gmail.com>
2014-11-30 03:43:30 +0600

    Fixes: 1. Now you can do several detections. (Creates new ObjectRecognitionTask each time, preloads OpenCV resources onCreate()) 2. Works with any resolution (Decodes smaller bitmap from file)

--- android/src/kz/edu/nu/sst/quickshot/MainActivity.java
+++ android/src/kz/edu/nu/sst/quickshot/MainActivity.java
@@ -13,7 +13,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -25,23 +24,92 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	protected static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_1 = 100;
-	protected static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_2 = 101;
 	Button imageButton;
 	ImageView imageView;
-	Bitmap image1;
+	Bitmap image;
 	Uri mImageUri1;
 	TextView textView;
 
-	Bitmap image;
 
 	TextView tv;
 	PlaceList placeList = null;
 
-	ObjectRecognitionTask task;
+	
+	public void initOpenCV() {
+		if (OpenCVTool.initialized)
+			return;
+		String [] classNames = new String [] {"congresshall", "shabyt", "vokzal", "hanshatyr", "triumf",
+                "baiterek", "pyramid", "keruyen", "defence", "nuniversity"};
+		
+		PlaceCV [] places = new PlaceCV [classNames.length];
+		
+		byte[] buffer = new byte[1024];
+		
+		String pathToVocabulary = null;
+		InputStream inputStream ;
+		OutputStream outputStream = null ;
+		try {
+			inputStream = this.getAssets().open("vocabulary.yml");
+			File vocabularyFile = createTemproraryFile("vocabulary", "yml");
+			outputStream = new BufferedOutputStream(new FileOutputStream(vocabularyFile)); 
+			pathToVocabulary = vocabularyFile.getAbsolutePath();
+	        int length = 0;
+	        try {
+	            while ((length = inputStream.read(buffer)) > 0){
+	                outputStream.write(buffer, 0, length);
+	            }
+	        } catch (IOException ioe) {
+	            /* ignore */		
+	        }
+			
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		} finally {
+			if (outputStream != null) {
+				try {
+					outputStream.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		for (int i = 0 ; i < classNames.length ; i ++) {
+			Log.d("MainActivity", "i = " + i);
+			int length = 0;
+			try {
+				inputStream = getAssets().open(classNames[i] + ".xml");
+
+				File currentFile = createTemproraryFile(classNames[i], "xml");
+				places[i] = new PlaceCV(classNames[i], currentFile.getAbsolutePath());
+				
+				outputStream = new BufferedOutputStream(new FileOutputStream(
+						currentFile));
+				while ((length = inputStream.read(buffer)) > 0) {
+					outputStream.write(buffer, 0, length);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				if (outputStream != null)
+					try {
+						outputStream.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			}
+		}
+		
+		OpenCVTool.initializePlacesForTraining(places, pathToVocabulary);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -76,94 +144,7 @@ public class MainActivity extends Activity {
 		});
 		textView = (TextView) findViewById(R.id.textView1);
 		
-		String [] arr = new String [] {"congresshall.xml", "shabyt.xml", "vokzal.xml", "hanshatyr.xml", "triumf.xml",
-                "baiterek.xml",		"pyramid.xml",
-                "keruyen.xml", "defence.xml", "nuniversity.xml"};
-		byte[] buffer = new byte[1024];
 		
-		String pathToVocabulary = null;
-		InputStream inputStream ;
-		OutputStream outputStream = null ;
-		try {
-			inputStream = this.getAssets().open("vocabulary.yml");
-			File vocabularyFile = createTemproraryFile("vocabulary", "yml");
-			outputStream = new BufferedOutputStream(new FileOutputStream(vocabularyFile)); 
-			pathToVocabulary = vocabularyFile.getAbsolutePath();
-	        int length = 0;
-	        try {
-	            while ((length = inputStream.read(buffer)) > 0){
-	                outputStream.write(buffer, 0, length);
-	            }
-	        } catch (IOException ioe) {
-	            /* ignore */
-	        }
-			
-		} catch (IOException e2) {
-			e2.printStackTrace();
-		} finally {
-			if (outputStream != null) {
-				try {
-					outputStream.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		String [] classNames = new String [arr.length];
-		
-		for (int i = 0 ; i < arr.length ; i ++) {
-			Log.d("MainActivity", "i = " + i);
-			int length = 0;
-			try {
-				inputStream = getAssets().open(arr[i]);
-				classNames[i] = arr[i].substring(0, 
-						arr[i].indexOf('.'));
-				File currentFile = createTemproraryFile(classNames[i], "xml");
-				arr[i] = currentFile.getAbsolutePath();
-				outputStream = new BufferedOutputStream(new FileOutputStream(
-						currentFile));
-				while ((length = inputStream.read(buffer)) > 0) {
-					outputStream.write(buffer, 0, length);
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				if (outputStream != null)
-					try {
-						outputStream.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-			}
-			
-			
-		}
-		
-		task = new ObjectRecognitionTask(textView, pathToVocabulary, arr, classNames);
-		
-
-		// Button saveButton = new Button(this);
-		// saveButton.setText("Start detection!");
-		//
-		// saveButton.setOnClickListener(new View.OnClickListener() {
-		//
-		// @Override
-		// public void onClick(View v) {
-		// if (image1 != null && image2 != null) {
-		// imageView.setImageBitmap(image);
-		// ObjectRecognitionTask task = new ObjectRecognitionTask(
-		// imageView);
-		// task.execute(image1, image2);
-		// // ObjectRecognizer detector = new ObjectRecognizer(image1,
-		// // image2, image);
-		// // new Thread(detector).start();
-		// }
-		// }
-		// });
 
 		InputStream in = null;
 		try {
@@ -178,11 +159,14 @@ public class MainActivity extends Activity {
 			e.printStackTrace();			
 		}
 
+		initOpenCV();
+		Toast toast = Toast.makeText(getApplicationContext(), "OpenCV tools were inited", Toast.LENGTH_SHORT);
+		toast.show();
 	}
 
 	private File createTemproraryFile(String part, String ext)
 			throws IOException {
-		File tempDir = Environment.getExternalStorageDirectory();
+		File tempDir = Environment.getExternalStorageDirectory();		
 		tempDir = new File(tempDir.getAbsolutePath() + "/.temp");
 		if (!tempDir.exists()) {
 			tempDir.mkdir();
@@ -210,65 +194,22 @@ public class MainActivity extends Activity {
 		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_1
 				&& resultCode == RESULT_OK) {
 
-			// this.getContentResolver().notifyChange(mImageUri1, null);
-			// ContentResolver cr = this.getContentResolver();
 			Bitmap photo;
 
-			// photo = android.provider.MediaStore.Images.Media.getBitmap(cr,
-			// mImageUri1);
-			photo = decodeSampledBitmapFromFile(mImageUri1.getPath(), 480, 640);
+			photo = OpenCVTool.decodeSampledBitmapFromFile(mImageUri1.getPath(), 480, 640);
 			Log.d("RESULT", "Path = " + mImageUri1.getPath());
-			image1 = photo;
+			image = photo;
 			imageView.setImageBitmap(photo);
 			
 			/********** for testing purposes *************/
 			//displayResults("shabyt");
 			/*********************************************/
+			ObjectRecognitionTask task = new ObjectRecognitionTask(textView);
 			task.execute(mImageUri1.getPath());
-			
 		}
 	}
 
-	public static int calculateInSampleSize(BitmapFactory.Options options,
-			int reqWidth, int reqHeight) {
-		// Raw height and width of image
-		final int height = options.outHeight;
-		final int width = options.outWidth;
-		int inSampleSize = 1;
-
-		if (height > reqHeight || width > reqWidth) {
-
-			final int halfHeight = height / 2;
-			final int halfWidth = width / 2;
-
-			// Calculate the largest inSampleSize value that is a power of 2 and
-			// keeps both
-			// height and width larger than the requested height and width.
-			while ((halfHeight / inSampleSize) > reqHeight
-					&& (halfWidth / inSampleSize) > reqWidth) {
-				inSampleSize *= 2;
-			}
-		}
-
-		return inSampleSize;
-	}
-
-	public static Bitmap decodeSampledBitmapFromFile(String pathName,
-			int reqWidth, int reqHeight) {
-
-		// First decode with inJustDecodeBounds=true to check dimensions
-		final BitmapFactory.Options options = new BitmapFactory.Options();
-		options.inJustDecodeBounds = true;
-		BitmapFactory.decodeFile(pathName, options);
-
-		// Calculate inSampleSize
-		options.inSampleSize = calculateInSampleSize(options, reqWidth,
-				reqHeight);
-
-		// Decode bitmap with inSampleSize set
-		options.inJustDecodeBounds = false;
-		return BitmapFactory.decodeFile(pathName, options);
-	}
+	
 
 	public void displayResults(String id) {
 		Place place = null;
--- android/src/kz/edu/nu/sst/quickshot/ObjectRecognitionTask.java
+++ android/src/kz/edu/nu/sst/quickshot/ObjectRecognitionTask.java
@@ -2,17 +2,12 @@ package kz.edu.nu.sst.quickshot;
 
 import java.lang.ref.WeakReference;
 
-import org.bytedeco.javacpp.Loader;
-import org.bytedeco.javacpp.Pointer;
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.bytedeco.javacpp.opencv_core.Mat;
-import org.bytedeco.javacpp.opencv_features2d.BOWImgDescriptorExtractor;
-import org.bytedeco.javacpp.opencv_features2d.FlannBasedMatcher;
 import org.bytedeco.javacpp.opencv_features2d.KeyPoint;
-import org.bytedeco.javacpp.opencv_ml.CvSVM;
-import org.bytedeco.javacpp.opencv_nonfree.SIFT;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
@@ -20,72 +15,61 @@ import android.widget.TextView;
 public class ObjectRecognitionTask extends AsyncTask<String, Void, String> {
 	
 	private final WeakReference<TextView> textViewReference;
-	Mat vocabulary;
-	CvSVM [] classifiers;
-	String [] className;
-	SIFT detector;
-	FlannBasedMatcher matcher;
-	BOWImgDescriptorExtractor bowide;
+	private OpenCVTool instance;
+	
+	private final static int WORKING_WIDTH = 480, WORKING_HEIGHT = 640; 
 	
 	
 	
 	// Load classifiers and vocabulary to the memory
-	public ObjectRecognitionTask(TextView imageView, String pathToVocabulary, String [] pathToClassifiers,
-			String [] correspondingClassNames) 
+	public ObjectRecognitionTask(TextView imageView) 
 	{
+		instance = OpenCVTool.getInstance();
 		textViewReference = new WeakReference<TextView>(imageView);
-		Loader.load(opencv_core.class);
-        opencv_core.CvFileStorage storage = opencv_core.cvOpenFileStorage(pathToVocabulary, null, opencv_core.CV_STORAGE_READ);
-        Pointer p = opencv_core.cvReadByName(storage, null, "vocabulary", opencv_core.cvAttrList());
-        opencv_core.CvMat cvMat = new opencv_core.CvMat(p);
-        vocabulary = new opencv_core.Mat(cvMat);
-        Log.d("OpenCV", "vocabulary loaded " + vocabulary.rows() + " x " + vocabulary.cols());
-        
-        classifiers = new CvSVM [pathToClassifiers.length];
-        className = correspondingClassNames;
-        for (int i = 0 ; i < pathToClassifiers.length ; i++) {
-			Log.d("OpenCV", "Ok. Creating class name from "
-					+ pathToClassifiers[i]);
-        	classifiers[i] = new CvSVM();
-        	classifiers[i].load(pathToClassifiers[i]);
-        }
-        
-        detector = new SIFT();
-        matcher = new FlannBasedMatcher();
-        
-        bowide =
-                new BOWImgDescriptorExtractor(detector.asDescriptorExtractor(), matcher);
-
-        bowide.setVocabulary(vocabulary);
-        Log.d("OpenCV", "Vocab is set");
 	}
 
 	@Override
 	protected String doInBackground(String... arg0) {
-		IplImage image = org.bytedeco.javacpp.opencv_highgui.cvvLoadImage(arg0[0]);
+		Log.d("RecognitionTask", "Started");
+		Bitmap bitmapImage = OpenCVTool.decodeSampledBitmapFromFile(arg0[0], WORKING_WIDTH, WORKING_HEIGHT);
+		
+		//IplImage image = org.bytedeco.javacpp.opencv_highgui.cvvLoadImage(arg0[0]);
+		int w = bitmapImage.getWidth(), h = bitmapImage.getHeight();
+		
+		Log.d("RecognitionTask", "w, h = " + w + ", " + h);
+		
+		IplImage initialImage = IplImage.create(w, h, opencv_core.IPL_DEPTH_8U, 4);
+		
+		bitmapImage.copyPixelsToBuffer(initialImage.getByteBuffer());
+		
+		IplImage image = IplImage.create(w, h, opencv_core.IPL_DEPTH_8U, 3);
+		
+		org.bytedeco.javacpp.opencv_imgproc.cvCvtColor(initialImage, image,
+				org.bytedeco.javacpp.opencv_imgproc.CV_RGBA2BGR);
+		
 		
 		KeyPoint keypoints = new KeyPoint();
 		
 		Mat input = new Mat(image);
-		detector.detect(input, keypoints);
+		instance.detector.detect(input, keypoints);
 		
 		Mat response_hist = new Mat();
         
 
-        bowide.compute(input, keypoints, response_hist);
+		instance.bowide.compute(input, keypoints, response_hist);
 
 
         float minf = Float.MAX_VALUE;
         String bestMatch = null;
-        for (int i = 0 ; i < className.length ; i ++) {
-            float res = classifiers[i].predict(response_hist,true);
-            System.out.println("res = " + res);
+        for (int i = 0 ; i < instance.places.length ; i ++) {
+            float res = instance.classifiers[i].predict(response_hist,true);
             if (res < minf) {
                 minf = res;
-                bestMatch = className[i];
+                bestMatch = instance.places[i].className;
             }
         }
         Log.d("OpenCV", "detected " + bestMatch);
+        image.release();
 		return bestMatch;
 		
 	}
